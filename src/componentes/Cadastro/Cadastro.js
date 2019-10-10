736143263006a1d30f176210b79f687904990653
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-10-10 17:39:18 -0300

    Finalizar cadastro

--- src/componentes/Cadastro/Cadastro.js
+++ src/componentes/Cadastro/Cadastro.js
@@ -9,6 +9,8 @@ const Cadastro = () => {
   const [confirmEmail, setConfirmEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [id, setId] = useState(1);
+  const [count, setCount] = useState (1);
+  const [mensagem, setMensagem] = useState("")
 
   const gerarPersonagem = () => {
     if (id === 494){
@@ -18,22 +20,36 @@ const Cadastro = () => {
     }
   }
 
+  const resposta = (texto) => {
+    setMensagem(texto);
+      setTimeout(()=>{
+        setMensagem("")
+    }, 2000)
+  }
+
   const handleSubmit = e => {
     e.preventDefault();
-    const payload = {
-      name: nome,
-      email: email,
-      confirm_email: confirmEmail,
-      password: senha
+    if (email === confirmEmail){
+      const payload = {
+        name: nome,
+        email: email,
+        confirm_email: confirmEmail,
+        password: senha
+      }
+      localStorage.setItem(`Dados${count}`, JSON.stringify(payload));
+        setCount(count + 1);
+      setNome("");
+      setEmail("");
+      setConfirmEmail("");
+      setSenha("");
+      resposta("Cadastro realizado com sucesso");
+    }else {
+      resposta("Os emails não correspondem")
+      setTimeout(()=>{
+        setMensagem("")
+      }, 2000)
     }
-    setNome("");
-    setEmail("");
-    setConfirmEmail("");
-    setSenha("");
-
-    console.log(payload)
-  };
-
+    }
 
   useEffect(()=>{
     fetch(`https://rickandmortyapi.com/api/character/${id}`, {
@@ -52,6 +68,7 @@ const Cadastro = () => {
     <div className="Cadastro">
       <h1>Faça seu cadastro</h1>
       <button onClick={gerarPersonagem}>Gerar Personagem</button>
+      <p>{mensagem}</p>
       <form onSubmit={handleSubmit}>
         <Input
           value={nome}
