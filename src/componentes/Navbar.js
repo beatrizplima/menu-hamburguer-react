e6dea5f102a2059e5d6266c77a955a08832d0e99
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-10-11 17:51:04 -0300

    alteracao

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import Produtos from './Produtos';
-import Pagamento from './Pagamento';
-import Carrinho from './Carrinho';
-import Cadastro from './Cadastro/Cadastro';
+
 
 
 
--- src/componentes/Produtos.js
+++ src/componentes/Produtos.js
@@ -5,25 +5,42 @@ import './Produtos.scss';
 
 
 const Produtos = () => {
+    const [numero,setNumero] = useState(0);
+
+    const incrementar = () => {
+        if (numero <10){
+            setNumero(numero + 1);
+        }
+    }
+    const descrementar = () => {
+        if (numero > 0){
+            setNumero(numero - 1)
+        }
+    }
+
+
+
     const [texto, setTexto] = useState("")
-     
+
     useEffect(() => {
-        if(localStorage.getItem("Name")){ 
-           const nomeLocal =  localStorage.getItem("Name");
-            setTexto (nomeLocal);
-        }   
-    }) 
+        if (localStorage.getItem("Name")) {
+            const nomeLocal = localStorage.getItem("Name");
+            setTexto(nomeLocal);
+        }
+    })
     return (
-        <div>
-        <h2>{`Seja bem vindo ${texto}`}</h2>
-        <h3></h3>
-       
-            <div className='imagem1'></div>
-            <div>Tenis nike do futuro</div>
-            <button>+</button>
-            <button>-</button>
-            <button>Comprar</button>
-        
+        <div className="Produtos">
+            <h2>{`Seja bem vindo ${texto}`}</h2>
+            <h3></h3>
+
+            <div className='imagemNike'></div>
+            <div className="nome">Tenis nike do futuro</div>
+            <div>preço:x</div>
+            <div>{numero}</div>
+            <button className="botaoMaior" onClick={incrementar}>+</button>
+            <button className="botaoMenor" onClick={descrementar}>-</button>
+            <button className="compra">Comprar</button>
+
         </div>
     )
 }
